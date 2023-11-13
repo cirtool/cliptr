7bc54cfba68f8d448d80d52f3e600eca80368b8b
Luis Medina <git@lmedinam.es>
2023-11-13 10:10:28 -0400

    Set systray correct app name

--- main.go
+++ main.go
@@ -16,15 +16,15 @@ func main() {
 }
 
 func onReady() {
-	systray.SetTitle("Awesome App")
-	systray.SetTooltip("Lantern")
+	systray.SetTitle("Cliptr")
+	systray.SetTooltip("Cliptr v0.1.0")
 
 	handler.AddAction("Trim", "Remove Spaces at start and end of string", Trim)
 	handler.AddAction("Capitalize Uppercase", "Capitalize each word if value is uppercase", CapitalizeUppercase)
 
 	systray.AddSeparator()
 
-	mQuitOrig := systray.AddMenuItem("Quit", "Quit the whole app")
+	mQuitOrig := systray.AddMenuItem("Quit", "Close the application")
 	go func() {
 		<-mQuitOrig.ClickedCh
 		systray.Quit()
